fbdd8f70544dcd04ae8230cc4497439b7131e8d2
Borja Armet <borjaarmet@gmail.com>
2021-04-15 15:08:08 +0200

    lab done. Not done the iteration 6,I'm stuck

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res) => res.render('index'));
+
+app.get('/Beers', (req, res) => {
+    punkAPI.getBeers()
+        .then(beersArray => {
+            console.log(beersArray);
+            res.render('Beers', beersArray);
+        });
 });
+
+app.get('/Random', (req, res) => {
+    punkAPI.getRandom()
+        .then(randomBeer => {
+            console.log(randomBeer[0]);
+            res.render('Random-beer', randomBeer[0]);
+        });
+});
+
+app.get('/Beers/:id', (req, res) => {
+    punkAPI.getBeer()
+        .then(beer => {
+            console.log(req.params);
+            res.render('beerDetails', { details: beer })
+        })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
